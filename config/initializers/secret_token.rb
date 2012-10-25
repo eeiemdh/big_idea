47cc9bcf94e63c128a3e19a5d39d00f1b9363dd1
Enda McDonagh <oisin123@eircom.net>
2012-10-25 16:45:44 +0100

    Beginning Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigIdea::Application.config.secret_token = '8e09b473e58fc6b975904a9de3fbebd47c8c2890b28d00d6db86d1650180fdad2c52e579db35d704b32a6d5cd99c3aa2027c89533eefa45821bcd3f206dfb245'
